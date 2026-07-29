81521db73905dfdffb2dc93ee8c85fac8ba3fc21
Mruga <blackcanary1340@gmail.com>
2026-07-29 21:19:06 +0530

    lists

--- list.py
+++ list.py
@@ -18,3 +18,21 @@ print(li)  # Output: Lithuania
 print(la)  # Output: Latvia
 print(es)  # Output: Estonia
 print(rest)  # Output: ['Norway', 'Finland', 'Sweden']
+
+fruits = ["orange", "cherry", "banana", "apple"]
+allfruits = fruits[0:4]
+print(allfruits)  # Output: ['orange', 'cherry', 'banana', 'apple']
+cb = fruits[1:3]
+print(cb)  # Output: ['cherry', 'banana']
+cba = fruits[1:4]
+print(cba)  # Output: ['cherry', 'banana', 'apple']
+ca = fruits[::2]
+print(ca)  # Output: ['orange', 'banana']
+all_fruits = fruits[-4:]
+print(all_fruits)  # Output: ['orange', 'cherry', 'banana', 'apple']
+cb = fruits[-3:-1]
+print(cb)  # Output: ['cherry', 'banana']
+cba = fruits[-3:]
+print(cba)  # Output: ['cherry', 'banana', 'apple']
+reverse = fruits[::-1]
+print(reverse)  # Output: ['apple', 'banana', 'cherry', 'orange']
