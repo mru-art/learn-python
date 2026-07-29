3daa743ea8ee370939b02c5bd4fe54484c977923
Mruga <blackcanary1340@gmail.com>
2026-07-29 21:08:26 +0530

    list

--- list.py
+++ list.py
@@ -1,2 +1,20 @@
 lst = []
 fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+f_fruit = fruits[0]
+print(f_fruit)  # Output: apple
+s_fruit = fruits[1]
+print(s_fruit)  # Output: banana
+
+# accessing elements using negative indexing
+l_fruit = fruits[-1]
+print(l_fruit)  # Output: elderberry
+sl_fruit = fruits[-2]
+print(sl_fruit)  # Output: date
+
+# unpacking the list items
+countries = ["Lithuania", "Latvia", "Estonia", "Norway", "Finland", "Sweden"]
+li, la, es, *rest = countries
+print(li)  # Output: Lithuania
+print(la)  # Output: Latvia
+print(es)  # Output: Estonia
+print(rest)  # Output: ['Norway', 'Finland', 'Sweden']
