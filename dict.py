5e7d8b4a29210a983f6055f99b05d4d3f904bc20
Mruga <blackcanary1340@gmail.com>
2026-07-31 19:46:58 +0530

    dict

---
+++ dict.py
@@ -0,0 +1,21 @@
+dict = {}
+
+dude = {
+    "fname": "James",
+    "lname": "LeBron",
+    "age": "46",
+    "county": "USA",
+    "address": {"street": "goat street", "house": "everybody's heart"},
+}
+
+print(len(dude))  # output = 5
+
+dude = {
+    "fname": "James",
+    "lname": "LeBron",
+    "age": "46",
+    "county": "USA",
+    "address": {"street": "goat street", "house": "everybody's heart"},
+}
+
+print(dude["James"])
