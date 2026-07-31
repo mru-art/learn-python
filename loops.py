805dc2bf144385cc137e8fa2d0437592b44debe1
Mruga <blackcanary1340@gmail.com>
2026-07-31 21:09:28 +0530

    loop

---
+++ loops.py
@@ -0,0 +1,20 @@
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+# prints from 0 to 4
+
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+else:
+    print(count)
+
+
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+    if count == 3:
+        break
