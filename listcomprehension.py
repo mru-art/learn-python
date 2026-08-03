08581f35ae36223498129b43b99cf46829508f75
Mruga <blackcanary1340@gmail.com>
2026-08-03 21:58:56 +0530

    com

---
+++ listcomprehension.py
@@ -0,0 +1,49 @@
+# syntax
+[expression for i in iterable if condition]
+
+# One way
+language = "Python"
+lst = list(language)  # changing the string to list
+print(type(lst))  # list
+print(lst)  # ['P', 'y', 't', 'h', 'o', 'n']
+
+# Second way: list comprehension
+lst = [i for i in language]
+print(type(lst))  # list
+print(lst)  # ['P', 'y', 't', 'h', 'o', 'n']
+
+
+# Generating numbers
+numbers = [i for i in range(11)]  # to generate numbers from 0 to 10
+print(numbers)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# It is possible to do mathematical operations during iteration
+squares = [i * i for i in range(11)]
+print(squares)  # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# It is also possible to make a list of tuples
+numbers = [(i, i * i) for i in range(11)]
+print(numbers)  # [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
+
+
+# combined if expression
+# Generating even numbers
+even_numbers = [
+    i for i in range(21) if i % 2 == 0
+]  # to generate even numbers list in range 0 to 21
+print(even_numbers)  # [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# Generating odd numbers
+odd_numbers = [
+    i for i in range(21) if i % 2 != 0
+]  # to generate odd numbers in range 0 to 21
+print(odd_numbers)  # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+# Filter numbers: let's filter out positive even numbers from the list below
+numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
+positive_even_numbers = [i for i in numbers if i % 2 == 0 and i > 0]
+print(positive_even_numbers)  # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# Flattening a two dimensional array
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list = [number for row in list_of_lists for number in row]
+print(flattened_list)  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
