f06cab447417896c5a000818775307718fd66105
Mruga <blackcanary1340@gmail.com>
2026-08-03 22:05:56 +0530

    lambda

--- listcomprehension.py
+++ listcomprehension.py
@@ -47,3 +47,58 @@ print(positive_even_numbers)  # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 flattened_list = [number for row in list_of_lists for number in row]
 print(flattened_list)  # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# LAMBDA FUNCTION
+# Named function
+def add_two_nums(a, b):
+    return a + b
+
+print(add_two_nums(2, 3))     # 5
+# Lets change the above function to a lambda function
+add_two_nums = lambda a, b: a + b
+print(add_two_nums(2,3))    # 5
+
+# Self invoking lambda function
+(lambda a, b: a + b)(2,3) # 5 - need to encapsulate it in print() to see the result in the console
+
+square = lambda x : x ** 2
+print(square(3))    # 9
+cube = lambda x : x ** 3
+print(cube(3))    # 27
+
+# Multiple variables
+multiple_variable = lambda a, b, c: a ** 2 - 3 * b + 4 * c
+print(multiple_variable(5, 5, 3)) # 22
+
+
+#lambda function inside another function
+def power(x):
+    return lambda n : x ** n
+
+cube = power(2)(3)   # function power now need 2 arguments to run, in separate rounded brackets
+print(cube)          # 8
+two_power_of_five = power(2)(5) 
+print(two_power_of_five)  # 32
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
